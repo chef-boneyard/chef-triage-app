394801c9b2042e169dfc90c52d0c1480633280d4
Thom May <thom@chef.io>
2016-08-27 16:58:27 +0100

    link to PRs and issues
    Signed-off-by: Thom May <thom@may.lt>

--- web/views/project_view.ex
+++ web/views/project_view.ex
@@ -8,4 +8,17 @@ defmodule Triage.ProjectView do
     end
   end
 
+  def issue_url(project, triaged \\ false) do
+    triage = if triaged do
+      "+no%3Amilestone"
+    else
+      ""
+    end
+    "https://github.com/#{project.organization}/#{project.name}/issues?q=is%3Aopen+is%3Aissue+sort%3Aupdated-asc#{triage}"
+  end
+  
+  def pr_url(project) do
+    "https://github.com/#{project.organization}/#{project.name}/pulls?q=is%3Aopen+is%3Apr+sort%3Aupdated-asc"
+  end
+  
 end
